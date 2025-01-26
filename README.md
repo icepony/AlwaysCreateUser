b547e94eea31e26e502e35de3f53c4b8da5778b0
icepony <44568304+icepony@users.noreply.github.com>
2025-01-26 13:51:04 +0800

    Feat: Support Android 4.2 - 13
    - Hook `isCreationOverrideEnabled` on Android 14+.
    - Hook `canAddMoreProfilesToUser` on Android 11 - 13.
    - Hook `isUserLimitReached` on Android 7 - 13.
    - Hook `canAddMoreManagedProfiles` on Android 6 - 10.
    - Hook `isUserLimitReachedLocked` on Android 4.2 - 6.
    - Update README.md with the new supported Android versions.

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Let `pm create-user` bypass some checks.
 Xposed Api 82
 
 `isCreationOverrideEnabled` Android 14+
+`canAddMoreProfilesToUser` Android 11 - 13 (Override by `isCreationOverrideEnabled`)
+`isUserLimitReached` Android 7 - 13 (Override by `isCreationOverrideEnabled`)
+`canAddMoreManagedProfiles` Android 6 - 10 (Move to `canAddMoreProfilesToUser`)
+`isUserLimitReachedLocked` Android 4.2 - 6 (Rename to `isUserLimitReached`)
 
 ## Screenshot
 
--- app/src/main/java/io/github/icepony/alwayscreateuser/MainHook.java
+++ app/src/main/java/io/github/icepony/alwayscreateuser/MainHook.java
@@ -16,11 +16,67 @@ public class MainHook implements IXposedHookLoadPackage {
         if (!lpparam.packageName.equals("android")) return;
         XposedBridge.log("D/" + TAG + " handleLoadPackage");
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) { // Android 14+
             XposedHelpers.findAndHookMethod(
                     "com.android.server.pm.UserManagerService",
                     lpparam.classLoader,
                     "isCreationOverrideEnabled",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                        }
+                    }
+            );
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) { // Android 11 - 13
+            XposedHelpers.findAndHookMethod(
+                    "com.android.server.pm.UserManagerService",
+                    lpparam.classLoader,
+                    "canAddMoreProfilesToUser",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                        }
+                    }
+            );
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) { // Android 7 - 13
+            XposedHelpers.findAndHookMethod(
+                    "com.android.server.pm.UserManagerService",
+                    lpparam.classLoader,
+                    "isUserLimitReached",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                        }
+                    }
+            );
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) { // Android 6 - 10
+            XposedHelpers.findAndHookMethod(
+                    "com.android.server.pm.UserManagerService",
+                    lpparam.classLoader,
+                    "canAddMoreManagedProfiles",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                        }
+                    }
+            );
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) { // Android 4.2 - 6
+            XposedHelpers.findAndHookMethod(
+                    "com.android.server.pm.UserManagerService",
+                    lpparam.classLoader,
+                    "isUserLimitReachedLocked",
                     new XC_MethodHook() {
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
