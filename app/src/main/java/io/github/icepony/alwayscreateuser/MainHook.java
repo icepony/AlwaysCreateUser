a420f73b1c57ee739837aa1646e3612011f81efa
icepony <44568304+icepony@users.noreply.github.com>
2025-01-27 22:12:06 +0800

    Feat: Refactor hooking logic and bump version to 1.3
    - Refactor hooking logic to separate hooks into individual methods for better organization and readability.
    - Bump versionCode to 4 and versionName to 1.3.
    - Log message is now printed in afterHookedMethod in isCreationOverrideEnabled hook.
    - The result of isUserLimitReached and isUserLimitReachedLocked is changed to false instead of true.

--- app/src/main/java/io/github/icepony/alwayscreateuser/MainHook.java
+++ app/src/main/java/io/github/icepony/alwayscreateuser/MainHook.java
@@ -16,83 +16,105 @@ public class MainHook implements IXposedHookLoadPackage {
         if (!lpparam.packageName.equals("android")) return;
         XposedBridge.log("D/" + TAG + " handleLoadPackage");
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) { // Android 14+
-            XposedHelpers.findAndHookMethod(
-                    "com.android.server.pm.UserManagerService",
-                    lpparam.classLoader,
-                    "isCreationOverrideEnabled",
-                    new XC_MethodHook() {
-                        protected void beforeHookedMethod(MethodHookParam param) {
-                            XposedBridge.log("D/" + TAG + " isCreationOverrideEnabled call");
-                        }
+        hook(lpparam);
+    }
 
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            param.setResult(true);
-                        }
+    private void hook_isCreationOverrideEnabled(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookMethod(
+                "com.android.server.pm.UserManagerService",
+                lpparam.classLoader,
+                "isCreationOverrideEnabled",
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("D/" + TAG + " isCreationOverrideEnabled call");
+                        param.setResult(true);
                     }
-            );
+                }
+        );
+    }
+
+    private void hook_canAddMoreProfilesToUser(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookMethod(
+                "com.android.server.pm.UserManagerService",
+                lpparam.classLoader,
+                "canAddMoreProfilesToUser",
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("D/" + TAG + " canAddMoreProfilesToUser call");
+                        param.setResult(true);
+                    }
+                }
+        );
+    }
+
+    private void hook_canAddMoreManagedProfiles(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookMethod(
+                "com.android.server.pm.UserManagerService",
+                lpparam.classLoader,
+                "canAddMoreManagedProfiles",
+                int.class,
+                boolean.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("D/" + TAG + " canAddMoreManagedProfiles call");
+                        param.setResult(true);
+                    }
+                }
+        );
+    }
+
+    private void hook_isUserLimitReached(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookMethod(
+                "com.android.server.pm.UserManagerService",
+                lpparam.classLoader,
+                "isUserLimitReached",
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("D/" + TAG + " isUserLimitReached call");
+                        param.setResult(false);
+                    }
+                }
+        );
+    }
+
+    private void hook_isUserLimitReachedLocked(XC_LoadPackage.LoadPackageParam lpparam) {
+        XposedHelpers.findAndHookMethod(
+                "com.android.server.pm.UserManagerService",
+                lpparam.classLoader,
+                "isUserLimitReachedLocked",
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("D/" + TAG + " isUserLimitReachedLocked call");
+                        param.setResult(false);
+                    }
+                }
+        );
+    }
+
+    private void hook(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) { // Android 14+
+            hook_isCreationOverrideEnabled(lpparam);
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) { // Android 11 - 13
-            XposedBridge.log("D/" + TAG + " Android 11 or above detected, Hook isCreationOverrideEnabled");
-            XposedHelpers.findAndHookMethod(
-                    "com.android.server.pm.UserManagerService",
-                    lpparam.classLoader,
-                    "canAddMoreProfilesToUser",
-                    new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            XposedBridge.log("D/" + TAG + " canAddMoreProfilesToUser call");
-                            param.setResult(true);
-                        }
-                    }
-            );
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) { // Android 7 - 13
-            XposedHelpers.findAndHookMethod(
-                    "com.android.server.pm.UserManagerService",
-                    lpparam.classLoader,
-                    "isUserLimitReached",
-                    new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            XposedBridge.log("D/" + TAG + " isUserLimitReached call");
-                            param.setResult(true);
-                        }
-                    }
-            );
+            hook_canAddMoreProfilesToUser(lpparam);
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) { // Android 6 - 10
-            XposedHelpers.findAndHookMethod(
-                    "com.android.server.pm.UserManagerService",
-                    lpparam.classLoader,
-                    "canAddMoreManagedProfiles",
-                    new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            XposedBridge.log("D/" + TAG + " canAddMoreManagedProfiles call");
-                            param.setResult(true);
-                        }
-                    }
-            );
+            hook_canAddMoreManagedProfiles(lpparam);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU) { // Android 7 - 13
+            hook_isUserLimitReached(lpparam);
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) { // Android 4.2 - 6
-            XposedHelpers.findAndHookMethod(
-                    "com.android.server.pm.UserManagerService",
-                    lpparam.classLoader,
-                    "isUserLimitReachedLocked",
-                    new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) {
-                            XposedBridge.log("D/" + TAG + " isUserLimitReachedLocked call");
-                            param.setResult(true);
-                        }
-                    }
-            );
+            hook_isUserLimitReachedLocked(lpparam);
         }
     }
 }
