90b34c46c4da3ddf8be4dd78b56d6a5d02569fcd
AraafRoyalll <araafroyalll@gmail.com>
2025-02-01 14:01:18 +0530

    Update MainHook.java (#7)
    I think logs are captured using XposedBridge , so there is no need of this import

--- app/src/main/java/io/github/icepony/alwayscreateuser/MainHook.java
+++ app/src/main/java/io/github/icepony/alwayscreateuser/MainHook.java
@@ -1,7 +1,6 @@
 package io.github.icepony.alwayscreateuser;
 
 import android.os.Build;
-import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
