cd4601713d3d6fe3fa2df31aac295fe1c8757421
icepony <44568304+icepony@users.noreply.github.com>
2025-04-24 22:28:34 +0800

    Update resources and manifest
    - Add `module_not_loaded` and `will_not_save` strings to `strings.xml`.
    - Add `xposedsharedprefs` metadata to `AndroidManifest.xml`.
    - Update `MainActivity.java` to check if the module is loaded and show an alert if not.
    - Add a `SettingsFragment` to `MainActivity.java` and show it by default.

--- app/src/main/java/io/github/icepony/alwayscreateuser/MainActivity.java
+++ app/src/main/java/io/github/icepony/alwayscreateuser/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.icepony.alwayscreateuser;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -8,6 +10,22 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        try {
+            getSharedPreferences(BuildConfig.APPLICATION_ID + "_preferences", Context.MODE_WORLD_READABLE);
+        } catch (SecurityException ignored) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.module_not_loaded)
+                    .setMessage(R.string.will_not_save)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show();
+        }
+
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, new SettingsFragment())
+                    .commit();
+        }
     }
 }
